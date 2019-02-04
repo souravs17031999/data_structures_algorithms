b3770dd3b968d54c669dfb40a8ac4eace62ec93b
Sourav kumar <33771969+souravs17031999@users.noreply.github.com>
2019-02-04 14:01:40 +0530

    @author sourav kumar , 04-02-2019

---
+++ linkedlists_product_knodes_addition.cpp
@@ -0,0 +1,82 @@
+// program to calculate sum of elements of linked lists and product for k nodes
+// @author sourav kumar , 04-02-2019
+#include<iostream>
+using namespace std;
+// structure for linked list
+struct node{
+    int data;
+    node *next;
+};
+// class to implement sum and product
+class linked_list{
+    private:
+    node *head;
+    public:
+    linked_list(){
+    head = NULL;}
+    // program for implementing insert nodes
+    void insert_node(int n){
+        node *new_node = new node;
+        new_node->data = n;
+        new_node->next = NULL;
+        if(head == NULL){
+            head = new_node;
+            return;
+    }
+        node *ptr = head;
+        while(ptr->next != NULL){
+            ptr = ptr->next;
+    }
+        ptr->next = new_node;
+}
+    // program for displaying linked lists
+    void display(){
+        if(head == NULL){
+            cout << "empty list";
+            return;
+    }
+        node *ptr = head;
+        while(ptr != NULL){
+            cout << ptr->data << "->";
+            ptr = ptr->next;
+    }
+        
+}
+    // program for implementing sum of element of linked lists
+    void sum(){
+        int sum = 0;
+        node *ptr = head;
+        while(ptr->next != NULL){
+            sum = sum + ptr->data;
+            ptr = ptr->next;
+    }
+        cout << endl << "sum of elements of linked list is " << sum+ptr->data << endl;
+}
+    // program for implementing product for elements of linked lists
+    void product(int k){
+        int prod = 1;
+        node *ptr = head;
+        for(int i = 1; i <= k; i++){
+            prod = prod*ptr->data;
+            ptr = ptr->next;
+    }
+        cout << "product is " << prod << endl;
+}
+};
+// driver fucntion for calling other functions
+int main(){
+    linked_list a;
+    int t,data;
+    cout << "tell no of nodes to insert" << endl;
+    cin >> t;
+    for(int i = 0; i <= t-1; i++){
+        cout << "enter data for node "<< i+1 << endl;
+        cin >> data;
+        a.insert_node(data);
+}
+    
+    a.display();
+    a.sum();
+    a.product(3);
+    
+}
