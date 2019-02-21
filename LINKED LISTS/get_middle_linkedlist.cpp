2ed8e9eac8d841f709c884bbd75e2460a85e1aae
Sourav kumar <33771969+souravs17031999@users.noreply.github.com>
2019-02-21 08:18:27 +0530

    update middle of linked lists

--- LINKED LISTS/get_middle_linkedlist.cpp
+++ LINKED LISTS/get_middle_linkedlist.cpp
@@ -1,6 +1,73 @@
 // program to get the middle of linked list 
-// @author sourav kumar , 12-02-2019
+// @author sourav kumar , 21-02-2019
 #include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node *next;
+};
+// class to implement linked list 
+class linked_list{
+    private:
+    node *head;
+    public:
+    linked_list(){head = NULL;}
+    // function to implement insertion of node at the end of linked list
+    void insert(int n){
+        node *new_node = new node;
+        new_node->data = n;
+        new_node->next = NULL;
+        if(head == NULL){
+           head = new_node;
+            return;
+    }
+        node *ptr = head;
+    while(ptr->next != NULL){
+    ptr = ptr -> next;
+}
+    ptr -> next = new_node;
+
+}   
+	// function to display the linked list 
+    void display(){
+        if(head == NULL){
+            cout << "empty list" << endl;
+            return;
+        }
+        node *ptr = head;
+        while(ptr != NULL){
+            cout << ptr->data  << " ";
+            ptr = ptr->next;
+        }
+        cout << endl;
+    }
+     	// function to get the middle of a linked list
+	/* logic here is that simply keep two pointers one fast and one slow , the fast one moves by two and other two moves by one and then 
+	when fast pointer reaches last node , then slow pointer will be at middle node.
+	*/
+    void middle_list(){
+        if(head == NULL){
+            cout << "empty list" << endl;
+            return;
+        }
+        node *f = head;
+        node *s = head;
+        while(f != NULL && f->next != NULL){
+            f = f->next->next;
+            s = s->next;
+        }
+        cout << s->data << endl;
+    }
+
+};
+int main(){
+    linked_list l;
+    l.display();
+    l.middle_list();
+}
+
+/* #include<iostream>
 using namespace std;
 struct node{
     int data;
@@ -78,4 +145,4 @@ int main(){
     int middle = a.middle_list(length); // getting the middle of linked list 
     a.display();
     cout << middle;
-}
+} */
