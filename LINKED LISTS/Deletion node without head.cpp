462ea6042bb54d398381f63bef777f6a297299b8
Sourav kumar <33771969+souravs17031999@users.noreply.github.com>
2019-04-01 18:57:26 +0530

    Deletion of node without head pointer

---
+++ LINKED LISTS/Deletion node without head.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node *next;
+    node(int data){
+    this->data = data;
+    this->next = NULL;
+}
+};
+class linkedlist{
+    public:
+    void insert(node *&head, int data){
+        node *new_node = new node(data);
+        if(head == NULL){
+            head = new_node;
+            return;
+    }
+        node *ptr = head;
+        while(ptr->next != NULL){
+            ptr = ptr->next;
+    }
+        ptr->next = new_node;
+}
+    void display(node *&head){
+        if(head == NULL){
+            return;
+    }
+        node *ptr = head;
+        while(ptr != NULL){
+            cout << ptr->data << " ";
+            ptr = ptr->next;
+    }
+        cout << endl;
+}
+    void deletion(node *&ptr){
+        if(ptr == NULL){
+            return;
+    }
+        if(ptr->next == NULL){
+            delete ptr;
+            return;
+    }
+        node *temp = ptr->next;
+        ptr->data = temp->data;
+        ptr->next = temp->next;
+        delete temp;
+        
+        
+        
+}
+};
+int main(){
+    linkedlist l;
+    node *head = NULL;
+    l.insert(head, 1);
+    l.insert(head, 2);
+    l.insert(head, 3);
+    l.display(head);
+    l.deletion(head->next);
+    l.display(head);
+}
